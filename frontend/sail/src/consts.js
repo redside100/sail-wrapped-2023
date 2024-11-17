4bfb54d1ba0b2a1b4b747cffe5540b390abbbc0f
Andrew Peng <andrewpeng00@gmail.com>
2024-11-16 22:43:42 -0500

    Change api url

--- frontend/sail/src/consts.js
+++ frontend/sail/src/consts.js
@@ -3,7 +3,7 @@ export const AUTH_URL =
     ? "https://discord.com/api/oauth2/authorize?client_id=1174821530623021128&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=code&scope=identify%20guilds"
     : "https://discord.com/oauth2/authorize?client_id=1174821530623021128&response_type=code&redirect_uri=https%3A%2F%2Fsw2023.redside.moe&scope=guilds+identify";
 
-export const API_BASE = process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://api.sw2023.redside.moe";
+export const API_BASE = process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://api-sw2023.redside.moe";
 export const DISCORD_CDN_BASE = "https://cdn.discordapp.com"
 
 export const LEADERBOARD_TAB = {
